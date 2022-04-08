27aacead06bf410576f0e05fbecf55055ad63642
JW30 <wagnerxjonathan@gmail.com>
2022-04-08 13:54:53 +0200

    Add Exception handling

--- day_26_nato_alphabet/main.py
+++ day_26_nato_alphabet/main.py
@@ -5,8 +5,12 @@ new_dict = {row["letter"]: row["code"] for (_, row) in data.iterrows()}
 
 print("**** Welcome to the NATO Phonetic Alphabet Translator")
 while True:
-    word = input("**** Please input a word: ")
+    word = input("**** Enter a word: ")
     if word == "quit":
         break
-    out = [new_dict[f"{letter}"] for letter in word.upper()]
-    print(f"**** Your word is spelled: {out}")
+    try:
+        out = [new_dict[f"{letter}"] for letter in word.upper()]
+        print(f"**** Your word is spelled: {out}")
+    except KeyError:
+        print("**** Sorry, only letters allowed.")
+
