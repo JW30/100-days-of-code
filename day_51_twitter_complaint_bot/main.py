32feb1f668596c79a27799c24bc80f94a1c06119
JW30 <wagnerxjonathan@gmail.com>
2022-06-18 11:59:36 +0200

    Remove unnecessary imports

--- day_51_twitter_complaint_bot/main.py
+++ day_51_twitter_complaint_bot/main.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
